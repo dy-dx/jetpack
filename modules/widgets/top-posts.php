264afa307497ba605dfd924b825cb35f6b9e16d9
Chris Kim <dy-dx@users.noreply.github.com>
2018-04-13 12:24:41 -0400

    workaround for missing top-posts widget thumbnails

--- modules/widgets/top-posts.php
+++ modules/widgets/top-posts.php
@@ -336,8 +336,8 @@ class Jetpack_Top_Posts_Widget extends WP_Widget {
 					$post['post_id'],
 					array(
 						'fallback_to_avatars' => true,
-						'width'               => (int) $width,
-						'height'              => (int) $height,
+						'width'               => null,
+						'height'              => null,
 						'avatar_size'         => (int) $get_image_options['avatar_size'],
 					)
 				);
